4c3c415e9c759366e534d33773f14ab59d620cbd
AsHansen88 <Ande970s@stud.kea.dk>
2022-08-30 21:45:01 +0200

    Virker næsten2

--- src/main/java/com/example/twonumbers/Controller/Homecontroller.java
+++ src/main/java/com/example/twonumbers/Controller/Homecontroller.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Homecontroller {
@@ -15,15 +16,15 @@ public class Homecontroller {
     return "Home/index";
   }
 
-  @GetMapping("/calculate")
+  @PostMapping("/calculate")
 
   public String Calculate(@ModelAttribute DTO data, Model model){
 
     String value_1= data.getValue_1();
     String value_2=data.getValue_2();
-    double convert=Double.parseDouble(value_1)+Double.parseDouble(value_2);
+    double result=Double.parseDouble(value_1)+Double.parseDouble(value_2);
 
-    model.addAttribute("result", 19);
+    model.addAttribute("result", result);
 
     return "home/calculate";
   }
